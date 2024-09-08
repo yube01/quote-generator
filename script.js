dcda6e89dd38f4bd22ab316b40a0140d3016ff46
yube01 <yubraja008@gmail.com>
2024-09-08 14:41:29 +0545

    prevoius and next button added

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 let quotes = [];
-let currentCategory = 'Science';
+let currentCategory = "Science";
 let currentIndex = 0;
 let filteredQuotes = [];
 
-const quoteDisplay = document.getElementById('quoteDisplay');
-const categorySelect = document.getElementById('categorySelect');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-const increaseFontBtn = document.getElementById('increaseFontBtn');
-const decreaseFontBtn = document.getElementById('decreaseFontBtn');
-const modeToggle = document.getElementById('modeToggle');
+const quoteDisplay = document.getElementById("quoteDisplay");
+const categorySelect = document.getElementById("categorySelect");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const increaseFontBtn = document.getElementById("increaseFontBtn");
+const decreaseFontBtn = document.getElementById("decreaseFontBtn");
+const modeToggle = document.getElementById("modeToggle");
 
 let currentFontSize = 16;
 
 async function loadQuotes() {
   try {
-    const response = await fetch('./quotes.json');
+    const response = await fetch("./quotes.json");
     const data = await response.json();
     quotes = data.quotes;
-    filteredQuotes = quotes.filter(quote => quote.category === currentCategory);
+    filteredQuotes = quotes.filter(
+      (quote) => quote.category === currentCategory
+    );
     displayQuote();
   } catch (error) {
     console.error("Error fetching quotes:", error);
@@ -34,6 +36,24 @@ function displayQuote() {
     quoteDisplay.innerHTML = "No quotes available.";
   }
 }
+
+nextBtn.addEventListener("click", () => {
+  if (currentIndex < filteredQuotes.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  displayQuote();
+});
+
+prevBtn.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = filteredQuotes.length - 1;
+  }
+  displayQuote();
+});
 
 
 loadQuotes();
